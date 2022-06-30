41486c93e769a61c3c34a3c0bbf0e180ad5f1ec6
Lívia Delgado <23265664+LivDelgado@users.noreply.github.com>
2022-06-30 16:51:46 +0000

    start implementing the message structuring

--- src/client/client.c
+++ src/client/client.c
@@ -9,12 +9,97 @@
 #include <netdb.h>
 
 #define MAXSTRINGLENGTH 500
+#define EXISTING_MESSAGES 8
+static const char *SPLITTER = " "; // character that represents the separation between words in the commands
 
 // prints the message sent in the parameter and exit with error status
 void printErrorAndExit(char *errorMessage)
 {
     puts(errorMessage);
     exit(1);
+}
+
+struct MessageFields
+{
+    int hasSourceId;
+    int hasDestineId;
+    int hasPayload;
+};
+
+struct Message
+{
+    int messageId;
+    int sourceId;
+    int destineId;
+    char *payload;
+};
+
+struct MessageFields *initializeMessageFields()
+{
+    struct MessageFields *messageFields = (struct MessageFields *)malloc(sizeof(struct MessageFields) * EXISTING_MESSAGES);
+
+    struct MessageFields REQ_ADD, REQ_REM, RES_ADD, RES_LIST, REQ_INF, RES_INF, ERROR, OK;
+
+    REQ_ADD.hasSourceId = 0;
+    REQ_ADD.hasDestineId = 0;
+    REQ_ADD.hasPayload = 0;
+
+    REQ_REM.hasSourceId = 1;
+    REQ_REM.hasDestineId = 0;
+    REQ_REM.hasPayload = 0;
+
+    RES_ADD.hasSourceId = 0;
+    RES_ADD.hasDestineId = 0;
+    RES_ADD.hasPayload = 1;
+
+    RES_LIST.hasSourceId = 0;
+    RES_LIST.hasDestineId = 0;
+    RES_LIST.hasPayload = 1;
+
+    REQ_INF.hasSourceId = 1;
+    REQ_INF.hasDestineId = 1;
+    REQ_INF.hasPayload = 0;
+
+    RES_INF.hasSourceId = 1;
+    RES_INF.hasDestineId = 1;
+    RES_INF.hasPayload = 1;
+
+    ERROR.hasSourceId = 0;
+    ERROR.hasDestineId = 1;
+    ERROR.hasPayload = 1;
+
+    OK.hasSourceId = 0;
+    OK.hasDestineId = 1;
+    OK.hasPayload = 1;
+
+    messageFields = (struct MessageFields[EXISTING_MESSAGES]){REQ_ADD, REQ_REM, RES_ADD, RES_LIST, REQ_INF, RES_INF, ERROR, OK};
+    return messageFields;
+}
+
+struct Message structureMessage(char *originalMessage)
+{
+    struct MessageFields *messageFields = initializeMessageFields();
+
+    struct Message structuredMessage;
+    int counter = 0;
+    char message[MAXSTRINGLENGTH] = "";
+    strcpy(message, originalMessage); // copy to avoid changing the message string
+
+    // first word is the id - it exists for every message
+    char *word = strtok(message, SPLITTER);
+    structuredMessage.messageId = atoi(word);
+    counter++;
+
+    while (word != NULL)
+    {
+        word = strtok(NULL, " "); // takes next word
+        if (word == NULL)
+            break;
+    }
+}
+
+char *destructureMessage(struct Message originalMessage)
+{
 }
 
 int createUdpSocket()
@@ -102,7 +187,7 @@ void sendMessageToServer(int clientSocket, char *message, struct addrinfo *servA
     puts("INFO: sent message to server");
 }
 
-void receiveMessageFromServer(int clientSocket)
+char *receiveMessageFromServer(int clientSocket)
 {
     puts("INFO: receiving message from server");
 
@@ -113,8 +198,15 @@ void receiveMessageFromServer(int clientSocket)
         printErrorAndExit("ERROR: recvfrom() failed");
     }
 
-    buffer[MAXSTRINGLENGTH] = '\0';
-    printf("INFO: received: %s\n", buffer);
+    return buffer;
+}
+
+void sendReqAdd(struct addrinfo *serverAddress, int clientSocket)
+{
+    char *reqAddMessage = "01";
+    sendMessageToServer(clientSocket, reqAddMessage, serverAddress);
+    char *responseFromServer = receiveMessageFromServer(clientSocket);
+    struct Message response = structureMessage(responseFromServer);
 }
 
 int main(int argc, char *argv[])
@@ -132,6 +224,9 @@ int main(int argc, char *argv[])
     // bindToBroadcasterServer(clientSocket, serverPort);
 
     struct addrinfo *serverAddress = getServerAddress(serverIpAddress, serverPort);
+
+    // send first connection message
+    sendReqAdd(serverAddress, clientSocket);
 
     while (1)
     {
@@ -152,7 +247,6 @@ int main(int argc, char *argv[])
         char *echoString = NULL; // create new message
         size_t echoStringLen;
 
-        printf("> ");
         getline(&echoString, &echoStringLen, stdin); // get the message from user input
         sendMessageToServer(clientSocket, echoString, serverAddress);
         receiveMessageFromServer(clientSocket);
